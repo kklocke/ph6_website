ca090715912c21dc80bcdf6cb403ae6b6e06759b
Kai Klocke <kai.c.klocke@gmail.com>
2018-01-08 23:33:24 -0800

    Javascript for adding navbar and footer. Some format changing

---
+++ footer.js
@@ -0,0 +1,29 @@
+document.write('\
+<footer class="text-center">\
+  <div class="card-footer">\
+    <div class="container">\
+      <div class="row">\
+        <div class="col-md-6">\
+          <h4>Frank Rice</h4>\
+          <p>Bridge 207 <br>PHONE <br> EMAIL </p>\
+        </div>\
+        <div class="col-md-6">\
+          <address>\
+            MSC ??? <br>\
+            1200 E. California Blvd. <br>\
+            Pasadena, CA 91125 <br>\
+          </address>\
+\
+        </div>\
+      </div>\
+    </div>\
+  </div>\
+  <div class="card-footer:last-child">\
+    <div class="container">\
+      <div class="row">\
+        <div class="col-lg-12"> All rights reserved &copy;</div>\
+      </div>\
+    </div>\
+  </div>\
+</footer>\
+');
---
+++ navbar.js
@@ -0,0 +1,32 @@
+document.write('\
+\
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">\
+  <div class="container">\
+    <a class="navbar-brand" href="#">Frank Rice</a>\
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">\
+      <span class="navbar-toggler-icon"></span>\
+  </button>\
+    <div class="collapse navbar-collapse" id="navbarResponsive">\
+      <ul class="navbar-nav ml-auto">\
+        <li class="nav-item active">\
+          <a class="nav-link" href="index.html">Home\
+            <span class="sr-only">(current)</span>\
+        </a>\
+    </li>\
+        <li class="nav-item">\
+          <a class="nav-link" href="curvefit.html">CurveFit</a>\
+      </li>\
+        <li class="nav-item">\
+          <a class="nav-link" href="ph5_home.html">Ph 5</a>\
+      </li>\
+        <li class="nav-item">\
+          <a class="nav-link" href="#">Ph 6</a>\
+      </li>\
+        <li class="nav-item">\
+          <a class="nav-link" href="#">Ph 7</a>\
+      </li>\
+  </ul>\
+  </div>\
+  </div>\
+</nav>\
+');
